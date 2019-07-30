99da3ace0f16fd562c347cef3c14c5537b0c1620
mutluhan <mutluhan43@gmail.com>
2019-07-30 17:05:28 +0300

    Updates

---
+++ precache-manifest.e25658a5272f4fc85481f3f42ae7895a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d012a8c117c82a3c463bea88f2dc13d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a9449fb91af4cc1130ec",
+    "url": "/weather-app/static/css/main.154b6bbd.chunk.css"
+  },
+  {
+    "revision": "6bcc389de612c6311cc7",
+    "url": "/weather-app/static/js/2.5e4a67b4.chunk.js"
+  },
+  {
+    "revision": "a9449fb91af4cc1130ec",
+    "url": "/weather-app/static/js/main.0bb10b72.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
